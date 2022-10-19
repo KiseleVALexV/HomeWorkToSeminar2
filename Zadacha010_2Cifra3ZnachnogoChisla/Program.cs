5bb5111670bbd6af62e612c5e28ff8a6fd4493ac
Alexey <manoil77@list.ru>
2022-10-19 13:04:35 +0300

    Add Zadacha010 Vtoraya cifra v trehznachnom chisle

---
+++ Zadacha010_2Cifra3ZnachnogoChisla/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int resault = (number / 10) % 10;
+
+Console.WriteLine($"Вторая цифра данного числа: {resault}");
+
+Console.ReadKey();
