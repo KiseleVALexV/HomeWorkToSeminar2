e71df07c1f49967f66972aeda6dffc6280682405
Alexey <manoil77@list.ru>
2022-10-23 13:52:46 +0300

    Add Zadacha015 Yavlyaetsya li den, oboznachenny cifroi, vyhodnym

---
+++ Zadacha015_YvlyaetsyaLiDenNedeliVyhodnym/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int number;
+Console.WriteLine("Введите число, обозначающее день недели(от 1 до 7) : ");
+string EntSign = Console.ReadLine();
+
+while (!Int32.TryParse(EntSign, out number) || number < 1 || number > 7)
+{
+    Console.WriteLine($"Неправильный ввод. Введите число, обозначающее день недели(от 1 до 7) : ");
+    EntSign = Console.ReadLine();
+}
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine($"День {number} - выходной");
+}
+else
+{
+    Console.WriteLine($"День {number} - не выходной");
+}
+Console.ReadKey();
